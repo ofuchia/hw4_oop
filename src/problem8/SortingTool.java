6637653f8e6e1b06768521c688349276196219b1
ofuchia <o.akpom@tcu.edu>
2023-12-02 21:29:10 -0600

    Commented something

--- src/problem8/SortingTool.java
+++ src/problem8/SortingTool.java
@@ -6,7 +6,7 @@ public class SortingTool {
     public static void main(String[] args) {
 
         //getting the proper Strategy
-        SortStrategy sorterObj = SorterFactoryofStrategies.getSorterStrategy(args[0]);
+        SortStrategy sorterObj = SorterFactoryofStrategies.getSorterStrategy(args[0]);  //programming to an interface so that's the type
         //doing the proper sort
         sorterObj.sortFile(args[0]);
 
